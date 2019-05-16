015f77a15584309e587f67f6aff8b5caa5a3d552
jonathanvdc <jonathan.vdc@outlook.com>
2019-05-16 17:30:46 +0200

    Add a test for try statements

---
+++ ts-closure-transform/test/fixture/try-statement.out.js
@@ -0,0 +1,12 @@
+var _a;
+let e = { value: undefined };
+e.value = 10;
+let f = (_a = function () {
+    ++e.value;
+    return e.value;
+}, _a.__closure = () => ({ e }), _a);
+try {
+}
+catch (e) {
+    console.log(e);
+}
